846920d491cf90f9f687152cd6d2e44b62eb72bc
Vui Nguyen <vui_engineer@yahoo.com>
2020-01-02 16:24:00 -0700

    add dummy API key for placeholder

---
+++ FishingDaze/Model/APIKeys.swift
@@ -0,0 +1,17 @@
+//
+//  APIKeys.swift
+//  FishingDaze
+//
+//  Created by Vui Nguyen on 1/2/20.
+//  Copyright © 2020 SunfishEmpire. All rights reserved.
+//
+
+import Foundation
+
+// Note: Replace the dummy API key value with your actual API Key
+// Sign up for a Weatherbit.io API key here:
+// https://www.weatherbit.io
+
+struct APIKeys {
+  static let WeatherBitIOKey = "abcdefg"
+}
